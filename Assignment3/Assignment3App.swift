102ee32a79ae388b3bfcd7360ebd568ce91402c4
quynguyn <s3878636@rmit.edu.vn>
2023-09-08 12:58:03 +0700

    Initial Commit

---
+++ Assignment3/Assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3App.swift
+//  Assignment3
+//
+//  Created by quy.nguyn on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
