4b50410d79a2e71b8b7591b3f45c34e2ce9d6e60
Duy Anh Pham <s3802674@student.rmit.edu.au>
2023-09-21 10:22:29 +0700

    Duy Anh: Navigate to StatusView after placing order & sort the order by date (newest order will be on the top)

---
+++ Assignment3/Models/MainFlowController.swift
@@ -0,0 +1,12 @@
+//
+//  MainFlowController.swift
+//  Assignment3
+//
+//  Created by Duy Anh Pham on 21/09/2023.
+//
+
+import Foundation
+
+class MainFlowController: ObservableObject {
+    @Published var selectedTab: Int = 0
+}
